1a3b384b0b2fb463e44f1c517218ad1faa2fcc29
Crowdbotics <engineering@crowdbotics.com>
2023-12-19 10:52:42 +0000

    Update store.

--- store/ravindersuperdevAPI/api.js
+++ store/ravindersuperdevAPI/api.js
@@ -1,72 +1,58 @@
-import axios from "axios";
+import axios from "axios"
 const ravindersuperdevAPI = axios.create({
   baseURL: "https://ravindersuper-dev-131803.botics.co",
-  headers: {
-    Accept: "application/json",
-    "Content-Type": "application/json"
-  }
-});
-
+  headers: { Accept: "application/json", "Content-Type": "application/json" }
+})
 function api_docs_schema_retrieve(payload) {
   return ravindersuperdevAPI.get(`/api-docs/schema/`, {
-    params: {
-      lang: payload.lang
-    }
-  });
+    params: { lang: payload.lang }
+  })
 }
-
 function api_v1_login_create(payload) {
-  return ravindersuperdevAPI.post(`/api/v1/login/`, payload);
+  return ravindersuperdevAPI.post(`/api/v1/login/`, payload)
 }
-
 function api_v1_signup_create(payload) {
-  return ravindersuperdevAPI.post(`/api/v1/signup/`, payload);
+  return ravindersuperdevAPI.post(`/api/v1/signup/`, payload)
 }
-
 function rest_auth_login_create(payload) {
-  return ravindersuperdevAPI.post(`/rest-auth/login/`, payload);
+  return ravindersuperdevAPI.post(`/rest-auth/login/`, payload)
 }
-
 function rest_auth_logout_create(payload) {
-  return ravindersuperdevAPI.post(`/rest-auth/logout/`);
+  return ravindersuperdevAPI.post(`/rest-auth/logout/`)
 }
-
 function rest_auth_password_change_create(payload) {
-  return ravindersuperdevAPI.post(`/rest-auth/password/change/`, payload);
+  return ravindersuperdevAPI.post(`/rest-auth/password/change/`, payload)
 }
-
 function rest_auth_password_reset_create(payload) {
-  return ravindersuperdevAPI.post(`/rest-auth/password/reset/`, payload);
+  return ravindersuperdevAPI.post(`/rest-auth/password/reset/`, payload)
 }
-
 function rest_auth_password_reset_confirm_create(payload) {
-  return ravindersuperdevAPI.post(`/rest-auth/password/reset/confirm/`, payload);
+  return ravindersuperdevAPI.post(`/rest-auth/password/reset/confirm/`, payload)
 }
-
 function rest_auth_registration_create(payload) {
-  return ravindersuperdevAPI.post(`/rest-auth/registration/`, payload);
+  return ravindersuperdevAPI.post(`/rest-auth/registration/`, payload)
 }
-
 function rest_auth_registration_resend_email_create(payload) {
-  return ravindersuperdevAPI.post(`/rest-auth/registration/resend-email/`, payload);
+  return ravindersuperdevAPI.post(
+    `/rest-auth/registration/resend-email/`,
+    payload
+  )
 }
-
 function rest_auth_registration_verify_email_create(payload) {
-  return ravindersuperdevAPI.post(`/rest-auth/registration/verify-email/`, payload);
+  return ravindersuperdevAPI.post(
+    `/rest-auth/registration/verify-email/`,
+    payload
+  )
 }
-
 function rest_auth_user_retrieve(payload) {
-  return ravindersuperdevAPI.get(`/rest-auth/user/`);
+  return ravindersuperdevAPI.get(`/rest-auth/user/`)
 }
-
 function rest_auth_user_update(payload) {
-  return ravindersuperdevAPI.put(`/rest-auth/user/`, payload);
+  return ravindersuperdevAPI.put(`/rest-auth/user/`, payload)
 }
-
 function rest_auth_user_partial_update(payload) {
-  return ravindersuperdevAPI.patch(`/rest-auth/user/`, payload);
+  return ravindersuperdevAPI.patch(`/rest-auth/user/`, payload)
 }
-
 export const apiService = {
   api_docs_schema_retrieve,
   api_v1_login_create,
@@ -82,4 +68,4 @@ export const apiService = {
   rest_auth_user_retrieve,
   rest_auth_user_update,
   rest_auth_user_partial_update
-};
+}
